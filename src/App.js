b1d368d15a2db389da307b3afaaaeef5f3329a43
Maher Jouda <moon_he2@hotmail.com>
2022-01-29 14:56:02 +0200

    Flex Box

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+      </div>
+      <div className="containerTwo">
+        <div className="row1">
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+        </div>
+        <div className="row1">
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+        </div>
+      </div>
+      <div className="containerFour">
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
